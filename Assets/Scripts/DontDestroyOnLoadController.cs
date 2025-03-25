c25ab2236dca1105aca6708cab8c52b4d85b2cc0
TharaneshA <tharanesh2k5@gmail.com>
2025-03-25 20:25:20 +0530

    Main Canvas Altered

--- Assets/Scripts/DontDestroyOnLoadController.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class DontDestroyOnLoadController : MonoBehaviour
-{
-    private void Awake()
-    {
-        DontDestroyOnLoad(gameObject); // ✅ Keep this object alive during scene transitions
-    }
-}
